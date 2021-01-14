4c49336e4c6fe367ddbcb25a734221522186d25b
WangXinyu <wxy990212@126.com>
2021-01-15 01:08:40 +0800

    优化代码

--- 1004/1004.cpp
+++ 1004/1004.cpp
@@ -1,29 +1,32 @@
 #include<iostream>
-#include <algorithm>
+#include<algorithm>
 using namespace std;
+
 struct stu
 {
-	char name[11];
+	char name[11]; //这里比最大位数大一，也许需要保存文件结尾标记
 	char no[11];
 	int score;
 };
-bool cmp(stu a,stu b)
+
+bool cmp(stu a, stu b) // 将stu结构体按照score排序
 {
-	return a.score > b.score;
+	return a.score < b.score; //从小到大
 }
 int main()
 {
 	int n;
 	cin >> n;
-	stu *s = new stu[n];
+	stu* stus = new stu[n];
 	for (int i = 0; i < n; i++)
 	{
-		cin >> s[i].name >> s[i].no >> s[i].score;
+		cin >> stus[i].name >> stus[i].no >> stus[i].score;
 	}
-	sort(s, s + n,cmp);
-	cout << s[0].name << ' ' << s[0].no  << endl;
-	cout << s[n - 1].name << ' ' << s[n - 1].no  << endl;
-	delete[] s;
-	system("pause");
+	sort(stus, stus + n, cmp);
+
+	cout << stus[n - 1].name << " " << stus[n - 1].no << endl;
+	cout << stus[0].name << " " << stus[0].no << endl;
+
+	delete[] stus;
 	return 0;
 }
