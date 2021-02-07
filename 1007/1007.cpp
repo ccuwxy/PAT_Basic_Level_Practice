33e69c3e7c6a0674e7695aa43c33e845c4eb0524
WangXinyu <wxy990212@126.com>
2021-02-07 11:03:33 +0800

    优化代码

--- 1007/1007.cpp
+++ 1007/1007.cpp
@@ -1,38 +1,74 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+
 int main()
 {
-	int n;
-	cin >> n;
+	int N, count = 0;
 	int temp = 0;
-	int* num = new int[n] {0};
+	cin >> N;
+	int* nums = new int[N+1]{ 0 };
 	int k = 0;
-	for (int i = 2; i <= n; i++)
+	for (int i = 2; i <= N; i++)
 	{
 		for (int j = 2; j <= sqrt(i); j++)
 		{
-			if (i%j == 0)
+			if (i % j == 0)
 			{
 				temp = 1;
 				break;
 			}
 		}
 		if (temp == 0)
-		{
-			num[k++] = i;
-		}
+			nums[i] = 1;
 		temp = 0;
 	}
-	int count = 0;
-	for (int i = 0; num[i+1] != 0; i++)
+	for (int i = 2; i <= N-2; i++)
 	{
-		if (num[i + 1] - num[i] == 2)
-		{
+		if (nums[i] == 1 && nums[i + 2] == 1)
 			count++;
-		}
 	}
-	cout << count << endl;
-	system("pause");
+	cout << count;
+	
+	delete []nums;
 	return 0;
 }
+
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int temp = 0;
+//	int* num = new int[n] {0};
+//	int k = 0;
+//	for (int i = 2; i <= n; i++)
+//	{
+//		for (int j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				temp = 1;
+//				break;
+//			}
+//		}
+//		if (temp == 0)
+//		{
+//			num[k++] = i;
+//		}
+//		temp = 0;
+//	}
+//	int count = 0;
+//	for (int i = 0; num[i + 1] != 0; i++)
+//	{
+//		if (num[i + 1] - num[i] == 2)
+//		{
+//			count++;
+//		}
+//	}
+//	cout << count << endl;
+//	system("pause");
+//	return 0;
+//}
